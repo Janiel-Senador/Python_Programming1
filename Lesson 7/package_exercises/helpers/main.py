59754e08c500e9bb0b493b3b385f99f51e89d52e
Janiel Senador <154493279+Janiel-Senador@users.noreply.github.com>
2025-10-05 01:37:57 +0800

    Delete Lesson 7/package_exercises directory

--- Lesson 7/package_exercises/helpers/main.py
+++
@@ -1,7 +0,0 @@
-from helpers import math as m, string as s
-
-length = 5
-width = 8
-result = area(length, width)
-#parag = f"the area of a {length}-by-{width} is {result}"
-print(s.shout(f"the area of a {length}-by-{width} is {result}"))
--- Lesson 7/package_exercises/helpers/string.py
+++
@@ -1,2 +0,0 @@
-def shout(txt):
-    return txt.upper()
